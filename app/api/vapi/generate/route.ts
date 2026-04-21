1464f39a10219cc413b4fb694bc26a3e4f2695d5
dushimire20 <odushimire922@daviscollege.com>
2026-04-21 12:37:28 +0200

    route.ts

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -149,7 +149,7 @@ export async function POST(request: Request) {
     let questionsText = "";
     try {
       const resp = await generateText({
-        model: google("gemini-2.0-flash-001"),
+        model: google("gemini-2.5-flash"),
         prompt: `Prepare questions for a job interview.
 The job role is ${role}.
 The job experience level is ${level}.
